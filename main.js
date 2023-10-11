465fcc8f3d23bedae3c3fc7456e5f22230d1cc04
Crispyray <146927602+Crispyray@users.noreply.github.com>
2023-10-11 14:24:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
-////----MAIN----////
-// Importer la bibliothèque Leaflet
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-
-// Importer la bibliothèque Leaflet Measure Control
-import 'leaflet-measure';
-
-// Importer le module de la carte
-import { initializeMap, addImageToMap, addPointsOfInterest, displayMap } from './map';
+import { initializeMap, addImageToMap, addPointsOfInterest, displayMap } from './map.js';
 
 // Appel de la fonction pour afficher la carte
 displayMap();
