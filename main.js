27bc42bbbc308bf5754e10f8b46e0bad88447221
Crispyray <146927602+Crispyray@users.noreply.github.com>
2023-10-11 13:33:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 ////----MAIN----////
+// Importer les modules
+import { initializeMap, addImageToMap, addPointsOfInterest, displayMap } from './modules/map.js';
+
+// Appel de la fonction pour afficher la carte
+displayMap();
